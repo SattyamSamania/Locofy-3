0740138aaace72432fa0891dd380a4f80f27284c
SattyamSamania <SattyamSamania>
2023-10-31 10:56:36 +0000

    Pushed from Locofy Builder

---
+++ src/components/DiagramFrontendAssignment.js
@@ -0,0 +1,7 @@
+const DiagramFrontendAssignment = () => {
+  return (
+    <div className="w-[1779px] h-[1482px] bg-[url('/public/diagram--frontend-assignment-1@3x.png')] bg-cover bg-no-repeat bg-[top]" />
+  );
+};
+
+export default DiagramFrontendAssignment;
